c5d03f7435f69802c081df4806fcb6188ff4d883
Luiz Ferreira <lgustavo.fonseca@gmail.com>
2012-02-15 13:46:21 +0000

    #432 - Otimizando sec_desk

--- src/wres.theme/wres/theme/skins/wres_theme_js/sec_desk.js
+++ src/wres.theme/wres/theme/skins/wres_theme_js/sec_desk.js
@@ -1,26 +1,27 @@
 
+function escondeMostraVisitas(){
+	var doctor_id = this.value;
+	if (this.selected == true) {
+		var selector = "tr." + doctor_id;
+		$(selector).fadeIn("slow");
+	}
+	else {
+		if(doctor_id != '') {
+			var selector = "tr." + doctor_id;
+			$(selector).fadeOut("slow");
+		}
+	}	
+}
+
 $(document).ready(function(){
 	
 	var show_visits_reloaded = false;
+	/*var fez_requisicao = false;*/
 	
 	$("#show_tomorrow_visits").hide();
 	
 	$("#doctor_select").change(function(){
 	
-		escondeMostraVisitas = function(){
-			var doctor_id = this.value;
-			if (this.selected == true) {
-				var selector = "tr." + doctor_id;
-				$(selector).fadeIn("slow");
-			}
-			else {
-				if(doctor_id != '') {
-					var selector = "tr." + doctor_id;
-					$(selector).fadeOut("slow");
-				}
-			}
-		}
-		
 		if($("option:selected").get(0).value == "") {
 			$("tr").fadeIn("slow");
 		}
@@ -61,26 +62,6 @@ $(document).ready(function(){
 	})	
 	
 	
-	$("a.change_state_link").click(function(event){
-        event.preventDefault();
-        
-        /* esconde links e mostra loader gif */
-        pai = $(this).parent();
-        avo = pai.parent();
-        avo.children("img").show();
-        pai.hide();
-        
-        href = $(this).attr('href');
-        $.post(href, function(data) {
-        	  if ($("#show_today_visits").is(":visible")) {
-        		  $("#show_today_visits").load(location.href + " #show_today_visits");
-        	  }
-        	  else {
-        		  $("#show_tomorrow_visits").load(location.href + " #show_tomorrow_visits");
-        	  }	        	
-        })
-	})
-
 	$(".show_visits").delegate("a.change_state_link", "click", function(event) {
         event.preventDefault();		
 
@@ -97,8 +78,8 @@ $(document).ready(function(){
         	  }
         	  else {
         		  $("#show_tomorrow_visits").load(location.href + " #show_tomorrow_visits");
-        	  }	        	
-        })        	
+        	  }
+        }) 
 	})	
 
 	$('.show_visits').ajaxComplete(function(e, xhr, settings) {
@@ -111,89 +92,6 @@ $(document).ready(function(){
 		}
 	})			
 
-/*	$('.show_visits').ajaxComplete(function(e, xhr, settings) {
-		
-		if($("option:selected").get(0).value == "") {
-			$("tr").fadeIn("slow");
-		}
-		else {
-			$("option").each(escondeMostraVisitas)
-		}			
-		
-		/* reassocia as funções com os eventos /
-		if (settings.url.indexOf("sec_desk") != -1 & show_visits_reloaded == false) {
-			$('a.visit_link').unbind('click');
-			$("a.visit_link").click(function(event){
-				event.preventDefault();
-				var data = {};
-				var url = this.href;
-				var extra = '/SFLight_visittemp_view';
-				var titulo = 'Detalhes da Consulta';
-				$dialog_content = $('#dialog_content');
-				$dialog_content.empty();
-				$dialog_content.dialog( "destroy" );
-				$.get(url+extra, data,
-				      function (msg) {
-				          $dialog_content.append(msg);
-				          $dialog_content.dialog({
-				            width: 'auto',
-				            autoOpen: true,
-				            modal: true,
-				            title: titulo,
-				          });
-				     }
-				);
-				
-			})
-			
-			$('a.patient_link').unbind('click');
-			$("a.patient_link").click(function(event){
-				
-				event.preventDefault();
-				var data = {};
-				var url = this.href;
-				var extra = '/SFLight_patient_view';
-				var titulo = 'Detalhes do Paciente';
-				$dialog_content = $('#dialog_content');
-				$dialog_content.empty();
-				$dialog_content.dialog( "destroy" );
-				$.get(url+extra, data,
-				      function (msg) {
-				          $dialog_content.append(msg);
-				          $dialog_content.dialog({
-				            width: 'auto',
-				            autoOpen: true,
-				            modal: true,
-				            title: titulo,
-				          });
-				     }
-				);
-				
-			})
-			
-			$('a.change_state_link').unbind('click');
-			$("a.change_state_link").click(function(event){
-		        event.preventDefault();
-		        
-		        /* esconde links e mostra loader gif /
-		        pai = $(this).parent();
-		        avo = pai.parent();
-		        avo.children("img").show();
-		        pai.hide();	
-		        
-		        href = $(this).attr('href');
-		        $.post(href, function(data) {
-		        	  if ($("#show_today_visits").is(":visible")) {
-		        		  $("#show_today_visits").load(location.href + " #show_today_visits");
-		        	  }
-		        	  else {
-		        		  $("#show_tomorrow_visits").load(location.href + " #show_tomorrow_visits");
-		        	  }	        	
-		        })
-			})			
-		}
-	});	*/
-	
 });
 
 function patientClick(){
