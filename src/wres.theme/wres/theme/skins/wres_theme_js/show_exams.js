d680f34fad3964ff275a8207db0fca200a42b164
Matheus Araujo <0matheus.araujo0@gmail.com>
2012-02-16 19:49:25 +0000

    #380 -> Template labs

---
+++ src/wres.theme/wres/theme/skins/wres_theme_js/show_exams.js
@@ -0,0 +1,160 @@
+//function show_note(){
+    //$("table#note_table").fadeIn();
+    //}
+//function hide_note(){
+    //$("table#note_table").fadeOut();
+    //}
+//function fill_hidden_end_date() {
+
+	//shown_end = $("input#shown_reported");
+	//hidden_end = $("input#hidden_reported");
+	//shown_end_date = shown_end.val();
+
+	//if(shown_end_date) {
+	//str_split = shown_end_date.split("/");
+	//day = str_split[0];
+	//month = str_split[1];
+	//year = str_split[2];
+
+	//end_f = year.concat("/", month, "/", day);
+
+	//hidden_end.val(end_f);
+	//}
+
+	//$(this).hide();
+	//$("#loadergif").show();		
+//}
+
+//function fill_hidden_start_date() {
+	//shown_start = $("input#started");
+	//hidden_start = $("input#hidden_started");
+	//shown_start_date = shown_start.val();
+
+	//if(shown_start_date) {
+		//str_split = shown_start_date.split("/");
+		//day = str_split[0];
+		//month = str_split[1];
+		//year = str_split[2];
+
+		//start_f = year.concat("/", month, "/", day);		
+
+		//hidden_start.val(start_f);
+	//}	
+//}
+
+//function date_higher_1900() {
+    //shown_start = $("input#started").val();
+    //str_split = shown_start.split("/");
+	//day = str_split[0];
+	//month = str_split[1];
+	//year = str_split[2];
+    //if(year < 1900){
+        //$("input#started").val("");
+        //document.getElementById('erro_date').style.display = "inline"
+        //}
+    //else document.getElementById('erro_date').style.display = "none"
+    
+//}
+
+//GLOBAL VARS
+var counter = 0
+var counter_trash = []
+
+function get_upper_row(row,n_rows){
+    var rows = $("#table_form")[0].rows
+    var before = 0
+    var row_id = null
+    for(i=1;i<=n_rows;i++){
+        row_id = parseInt(rows[i].id.split("linha")[1])
+        if (row == row_id){
+            return before
+            }
+            before = row_id
+        }
+    return null
+    }
+
+function remove_form_row(row){
+    var n_rows = $("#table_form")[0].rows.length
+    var upper_row = get_upper_row(row,n_rows)
+    
+    if(row == counter){
+        $("form").find("tbody").find('#linha'+row).remove();
+        //Devolve o poder de adicionar a linha acima unbind evita multiple fires
+        $("#exam_form"+upper_row).unbind('focus').bind("focus",add_form_row)
+        counter--
+        }
+    
+    else{
+        $("form").find("tbody").find('#linha'+row).remove();
+        counter_trash.push(row)
+        //Devolve o poder de adicionar a linha acima unbind evita multiple fires
+        if(upper_row == 0 && n_rows==3){
+                $("#exam_form"+upper_row).unbind('focus').bind("focus",add_form_row)
+            }
+        }
+    }
+
+function add_form_row(){
+    
+    counter++
+    var classe = ""
+    
+    
+    //Pintar fundo
+    if ((counter % 2) == 1){
+        classe = "odd"
+        }
+    else {
+        classe = ""
+        }
+    //Linha na tabela
+    
+    var hidden_input = "<input id />"
+    
+    var remove_button = "<td>"+
+                        "<span align='left'>"+
+                        "<b id='remove"+counter+"'><a href='javascript:void(0)' onClick='add_form_row()'>+&nbsp</a></b>"+
+                        "<b id='add"+counter+"'><a href='javascript:void(0)' onClick='remove_form_row("+counter+")'>&nbsp-</a></b>" +
+                        "</span></td>" 
+    var table_row = "<tr "+
+                            'id="linha'+counter+'"'+
+                            " class="+classe+">" +
+                            remove_button + 
+                            "<td>" +
+                                '<input id="exam_form'+counter+'"' +' type="text" name="exam_form'+counter+'"' +
+                                'onFocus="add_form_row()"' +
+                                'tal:attributes="value data/exam | nothing" /> '+
+                            '</td>'+
+                            '<td>'+
+                                '<input id="value_form'+counter+'"' +' type="text" name="value_form'+counter+'"' +
+                                'tal:attributes="value data/value | nothing" />'+
+                            '</td>'+
+                            '<td>'+
+                                '<input id="date_form'+counter+'"' +' type="text" name="date_form'+counter+'"' +' size="7"'+
+                                'tal:attributes="value data/date | python:context.getTodayDate().strftime('+"'%d/%m/%Y'" + ')"/>' +
+                            '</td>'+
+                        '</tr>"'
+    
+    $("form").find("tbody").append(table_row)
+    
+    //Remove poder de adcionar focus na linha acima
+    var n_rows = $("#table_form")[0].rows.length
+    $("#exam_form"+get_upper_row(counter,n_rows)).attr("onfocus","").unbind("focus")
+    
+    //Coloca a data do primeiro
+    
+    $("#date_form"+counter).val($("#date_form0").val())
+    
+    }
+
+
+$(document).ready(function(){
+    
+//  $("input#exam"+counter).focus(add_form_row)
+
+//	$("input#add_prescription").click(fill_hidden_end_date)
+
+//	$("input#add_prescription").click(fill_hidden_end_date)
+
+});
--- src/wres.theme/wres/theme/skins/wres_theme_scripts/save_exam.cpy
+++ src/wres.theme/wres/theme/skins/wres_theme_scripts/save_exam.cpy
@@ -1,14 +1,60 @@
 from DateTime import DateTime
 
-request = context.REQUEST
-vars = ['exam', 'value', 'date']
-exam = {}
-for var in vars:
-    exam[var] = request[var]
 
+request = context.REQUEST
+
+all_items = request.items()
+
+exams = []
+values = []
+dates = []
+all_exams_data = []
+
+#Esta funcao existe para proteger caso o request nao venha ordenado.
+def organize(exams,values,dates):
+    for tuple in exams:
+        number = tuple[0].split("exam_form")[1]
+        for tuple_date in dates:
+            if(tuple_date[0] == "date_form"+str(number)):
+                dates.remove(tuple_date)
+                dates.append(tuple_date)
+        for tuple_value in values:
+            if(tuple_value[0] == "value_form"+str(number)):
+                values.remove(tuple_value)
+                values.append(tuple_value)
+                
+
+                
+for tupla in all_items:
+    if("exam_form" in tupla[0]):
+        exams.append(tupla)
+    elif("value_form" in tupla[0]):
+        values.append(tupla)
+    elif("date_form" in tupla[0]):
+        dates.append(tupla)
+
+organize(exams,values,dates)
+
+
+pack = zip(exams,values,dates)
+exams_length = len(exams)
+
+for tuple in pack:
+    exam = {}
+    #Ignorar possivel ultimo campo vazio
+    if (tuple[0][1] == "" and tuple[1][1] == ""):
+        continue
+    exam["exam"] = tuple[0][1]
+    exam["value"] = tuple[1][1]
+    exam["date"] = tuple[2][1]
+    all_exams_data.append(exam)
+    
+
+#///////////////////////////////////////////////////////////////////////////////////////
 member = context.portal_membership.getAuthenticatedMember()
 exam['submitted_by'] = member.id
-context.saveLaboratory(**exam)
+for exam in all_exams_data:
+    context.saveLaboratory(**exam)
 state.set(portal_status_message='Exame adicionado.')
 return state
 
