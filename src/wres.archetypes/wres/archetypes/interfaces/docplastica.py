b211df830cdb1f42457202cde1c8545bf14430ac
Peter Thorun <prthorun@gmail.com>
2012-05-11 16:34:14 -0300

    #495 Modulo Archetypes limpo

--- src/wres.archetypes/wres/archetypes/interfaces/docplastica.py
+++
@@ -1,8 +0,0 @@
-from zope.interface import Interface
-# -*- Additional Imports Here -*-
-
-
-class IDocPlastica(Interface):
-    """A type of medical document"""
-
-    # -*- schema definition goes here -*-
--- src/wres.archetypes/wres/archetypes/interfaces/referringprovider.py
+++
@@ -1,8 +0,0 @@
-from zope.interface import Interface
-# -*- Additional Imports Here -*-
-
-
-class IReferringProvider(Interface):
-    """Type Referring Provider for WRES website"""
-
-    # -*- schema definition goes here -*-
