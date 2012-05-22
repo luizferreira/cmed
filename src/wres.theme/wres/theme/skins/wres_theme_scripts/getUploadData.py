549d8f32c878a216fe682a345e3d5ce72a8e7a7e
Matheus Araujo <0matheus.araujo0@gmail.com>
2012-05-21 21:01:58 -0300

    #503 Image Exibition Improved,#502 Tests Improved

---
+++ src/wres.theme/wres/theme/skins/wres_theme_scripts/getUploadData.py
@@ -0,0 +1,35 @@
+##bind context=context
+from Products.CMFCore.utils import getToolByName
+from DateTime import DateTime
+
+pc = getToolByName(context, 'portal_catalog')
+imagens = []
+other_files = []
+
+def getPatientId(context):
+    if context.meta_type == 'Patient':
+        return context.getId()
+    else:
+        return getPatientId(context.aq_inner.aq_parent) 
+
+def getFilesAndImages(context):
+    #Get Imagens
+    portal = context.getPortal()
+    patientPath = '/' + portal.getId() + '/Patients/' + getPatientId(context) + '/'
+    brains = pc.search({'portal_type': 'Image','path': patientPath})
+    for brain in brains:
+        imagePath = portal.absolute_url() + brain.getPath().replace("/"+portal.getId(),"")
+        date = brain.created.strftime("%y/%m/%d")
+        imagens.append((imagePath,date))
+
+    #Get Other Files
+    brains += pc.search({'portal_type': 'File','path': patientPath})
+    for brain in brains:
+        filePath = portal.absolute_url() + brain.getPath().replace("/"+portal.getId(),"")
+        name = brain.Title
+        icon = brain.getIcon
+        file = (filePath,name,icon)
+        other_files.append(file)
+    return [imagens,other_files]
+    
+return getFilesAndImages(context)
