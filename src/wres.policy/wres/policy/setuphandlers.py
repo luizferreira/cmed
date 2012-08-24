318daee328a7c4004d415f389ba70c4fcd2788fe
Luiz Ferreira <lgustavo.fonseca@gmail.com>
2012-08-24 15:45:27 -0300

    #631 - as informacoes do registre-se podem ser utilizadas como entrada para a criacao de uma instancia

--- src/wres.policy/wres/policy/setuphandlers.py
+++ src/wres.policy/wres/policy/setuphandlers.py
@@ -681,7 +681,17 @@ def addExampleTemplate(portal):
     consulta.setTitle("[Exemplo]Primeira Consulta")
     consulta.reindexObject()
     
-
+def parseFirstDoctorInputFile(infile):
+    lines = infile.readlines()
+    if len(lines) < 2:
+        return None
+    dic = {}
+    for i in range(len(lines)):
+        if i % 2 == 0:
+            lines[i] = lines[i].replace('\n', '')
+            lines[i+1] = lines[i+1].replace('\n', '')
+            dic[lines[i]] = lines[i+1]
+    return dic
 
 def setupVarious(context):
     """ Funcao generica executada na instalacao do wres policy """
@@ -702,6 +712,27 @@ def setupVarious(context):
 
     if context.readDataFile('wres.policy_various.txt') is not None:
         print '********************************ACHEI O TXT***********************************'   
+        # read firstdoctor_info and create a doctor if there is information there.
+        infile = context.openDataFile('firstdoctor_info.txt')
+        doctor_info = parseFirstDoctorInputFile(infile)
+        full_name = doctor_info['Nome Completo'].split(' ')
+        firstname = full_name[0].lower(); lastname = full_name[-1].lower()
+        doctor_id = firstname[0] + lastname
+
+        if doctor_info is not None:
+            doctor_folder = getattr(portal, 'Doctors')
+            clinic = getattr(portal, 'Clinic')
+            if not int(doctor_info['Quero o meu site profissional']):
+                # removing permissions from anonymous, so he cant see initial page anymore.
+                doctor_folder.manage_permission('Access contents information', [MANAGER_ROLE, UEMRADMIN_ROLE, DOCTOR_ROLE, SECRETARY_ROLE, TRANSCRIPTIONIST_ROLE, PATIENT_ROLE], acquire = False)
+                clinic.manage_permission('View', [MANAGER_ROLE, UEMRADMIN_ROLE, DOCTOR_ROLE, SECRETARY_ROLE, TRANSCRIPTIONIST_ROLE, PATIENT_ROLE, ANONYMOUS_ROLE], acquire = False)
+                doctor_folder.reindexObject()
+            doctor = getOrCreateType(portal, doctor_folder, doctor_id, 'Doctor')
+            doctor.fillFirstDoctorInfo(doctor_info)
+            clinic.fillClinicInformation(doctor_info)
+            doctor.reindexObject()
+            clinic.reindexObject()
+
         loadDocumentTypesVocabulary(portal)
         loadImpressoTypesVocaburary(portal)
         loadVisitVocabularies(portal)
