26a8452aa7fc3eadefebf9843a4eb4993f0043b6
Peter Thorun <prthorun@gmail.com>
2012-10-18 10:52:35 -0300

    Detalhe do chart_navigation, Consertado first_doctor Commmmmmuni

--- src/wres.theme/wres/theme/skins/wres_theme_js/chart.js
+++ src/wres.theme/wres/theme/skins/wres_theme_js/chart.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     var url = window.location.pathname;
     var id = url.split("/");
     id = "div#"+id[5];
+    if (id == "div#") id = "div#chart_folder_view";
     $(id).addClass("portlet_selected");
 
 });
