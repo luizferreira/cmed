11e35a3098e02b88a627ca19af06ec90e2512c71
Peter Thorun <prthorun@gmail.com>
2012-10-18 10:55:43 -0300

    Detalhe de highlight do chart_navigation

--- src/wres.theme/wres/theme/skins/wres_theme_js/chart.js
+++ src/wres.theme/wres/theme/skins/wres_theme_js/chart.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var url = window.location.pathname;
     var id = url.split("/");
     id = "div#"+id[5];
-    if (id == "div#") id = "div#chart_folder_view";
+    if (id == "div#" || id == "div#undefined") id = "div#chart_folder_view";
     $(id).addClass("portlet_selected");
 
 });
