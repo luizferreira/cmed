b3f0839d6150e78cef24dd191658f594834c9b22
matheusldaraujo <0matheus.araujo0@gmail.com>
2012-11-22 17:22:54 -0200

    #697 ChartFolder desativa o paciente

--- src/wres.theme/wres/theme/skins/wres_theme_scripts/patient_status_modify.py
+++ src/wres.theme/wres/theme/skins/wres_theme_scripts/patient_status_modify.py
@@ -12,7 +12,14 @@ action = request['workflow_action']
 if action not in ['activate','inactivate']:
 	container.REQUEST.RESPONSE.redirect(container.Patients.absolute_url() + "?portal_error_message=Não foi possível realizar a operação")	
 else:
-	context.doWorkflowAction(action)
+	if context.id == "chartFolder_hidden":
+		#Desativando paciente pelo prontuario
+		context = context.getParentNode()
+		context.doWorkflowAction(action)
+	else:
+		#Desativando paciente pelo view do paciente
+		context.doWorkflowAction(action)
+
 	if action == "activate":
 		container.REQUEST.RESPONSE.redirect(container.Patients.absolute_url() + "	?portal_status_message=" + context.getFullName() +  " foi reativado com sucesso")
 	else:	
