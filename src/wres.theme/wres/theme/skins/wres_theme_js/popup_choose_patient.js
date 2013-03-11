a77ef0b7208bd6321f7a85e3b2d2e05cab9f1237
Matheus Araujo <0matheus.araujo0@gmail.com>
2013-03-11 16:31:18 -0300

    Fix enter key bug in choose patient on visit schedule

--- src/wres.theme/wres/theme/skins/wres_theme_js/popup_choose_patient.js
+++ src/wres.theme/wres/theme/skins/wres_theme_js/popup_choose_patient.js
@@ -1,14 +1,22 @@
- function selectPatient(title, path){
-            document.body.style.cursor = 'progress';
-            window.opener.startPopulatingField(window, title, path);
-            setTimeout("window.close()", 100);
-        }
-        
-$(document).bind('keyup', function(e) {
-    if (e.which == 13) {
-        e.preventDefault();
-        li = $(".LSHighlight")
-        li.children("a").click()
-        window.close()
-    }
-});
+ function selectPatient(title, path) {
+     document.body.style.cursor = 'progress';
+     window.opener.startPopulatingField(window, title, path);
+     setTimeout("window.close()", 200);
+ }
+
+function stopEnterKey(evt) { 
+  var evt = (evt) ? evt : ((event) ? event : null); 
+  var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null); 
+  if ((evt.keyCode == 13) && (node.type=="text"))  {return false;} 
+} 
+document.onkeypress = stopEnterKey; 
+
+ $(document).ready(function(){
+  
+    $(document).bind('keyup', function(e) {
+         if (e.which == 13) {
+             li = $(".LSHighlight")
+             li.children("a").click()
+         }
+     });
+ });
