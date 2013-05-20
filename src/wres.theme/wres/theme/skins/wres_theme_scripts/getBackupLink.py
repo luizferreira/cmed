499adb4d1fdb52e3332b1c8da331c7e5c30ca1de
Matheus Araujo <0matheus.araujo0@gmail.com>
2013-05-20 15:34:13 -0300

    Add babkup to changes txt and fix small bug no template de  Ajuda

--- src/wres.theme/wres/theme/skins/wres_theme_scripts/getBackupLink.py
+++ src/wres.theme/wres/theme/skins/wres_theme_scripts/getBackupLink.py
@@ -1,4 +1,8 @@
 from Products.CMFCore.utils import getToolByName
 vt = getToolByName(context, 'vocabulary_tool')
 backup = vt.get_vocabulary('cmed_backup')
-return backup[0]
+
+try:
+	return backup[0]
+except:
+	return ""
