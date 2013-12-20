6e06a8f19796c217638700202d3650f60cac05be
Luiz Ferreira <lgustavo.fonseca@gmail.com>
2013-12-20 15:40:52 -0200

    #859 - Bug nos links do configuration_portlet

--- src/wres.theme/wres/theme/skins/wres_theme_scripts/configuration_portlet_script.py
+++ src/wres.theme/wres/theme/skins/wres_theme_scripts/configuration_portlet_script.py
@@ -5,22 +5,23 @@ response = {}
 auth_user = context.portal_membership.getAuthenticatedMember()
 user_roles = auth_user.getRoles()
 
-portal_url = context.portal_url.getPortalPath()
+cmed = context.restrictedTraverse(context.portal_url.getPortalPath())
+base = cmed.absolute_url()
 
 response['links'] = {}
-response['links']['configuration'] = portal_url + '/configuration'
-response['links']['doctorfolder'] = portal_url + '/Doctors'
-response['links']['secretaryfolder'] = portal_url + '/Secretaries'
+response['links']['configuration'] = base + '/configuration'
+response['links']['doctorfolder'] = base + '/Doctors'
+response['links']['secretaryfolder'] = base + '/Secretaries'
 
 if 'Doctor' in user_roles or 'Manager' in user_roles:
-    response['links']['clinic'] = portal_url + '/Clinic'
+    response['links']['clinic'] = base + '/Clinic'
 else:
     response['links']['clinic'] = ''
 
 if 'Doctor' in user_roles:
-    response['links']['mydata'] = portal_url + '/Doctors/' + auth_user.getId()
+    response['links']['mydata'] = base + '/Doctors/' + auth_user.getId()
 elif 'Secretary' in user_roles:
-    response['links']['mydata'] = portal_url + '/Secretaries/' + auth_user.getId()
+    response['links']['mydata'] = base + '/Secretaries/' + auth_user.getId()
 else:
     response['links']['mydata'] = None
 
