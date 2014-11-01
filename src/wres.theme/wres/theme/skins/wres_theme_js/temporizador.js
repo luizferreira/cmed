5c6b0a731f7ca0c546c52be92b54f89001ef5b7d
Luiz Ferreira <lgustavo.fonseca@gmail.com>
2014-11-01 08:26:58 -0200

    Retirando auto reload irritante do calendário

--- src/wres.theme/wres/theme/skins/wres_theme_js/temporizador.js
+++ src/wres.theme/wres/theme/skins/wres_theme_js/temporizador.js
@@ -1,19 +1,20 @@
-function temporizador(timer, elemento){
-    var conta = 0;
-    var tempo = window.setInterval(function(){
-        //$("#breadcrumbs-you-are-here").html(conta);
-        if(conta<=timer){
-            conta++;
-            $(elemento).mousemove(function(){
-                conta = 0;
-            });
-            $("body").keypress(function() {
-                conta = 0;
-            });
-        }else{
-            clearInterval(tempo);
-            window.location.reload();
-        }
-    }, 1000);
-}
-temporizador(300, "#content");
+// TODO: REMOVER em 2015
+// function temporizador(timer, elemento){
+//     var conta = 0;
+//     var tempo = window.setInterval(function(){
+//         //$("#breadcrumbs-you-are-here").html(conta);
+//         if(conta<=timer){
+//             conta++;
+//             $(elemento).mousemove(function(){
+//                 conta = 0;
+//             });
+//             $("body").keypress(function() {
+//                 conta = 0;
+//             });
+//         }else{
+//             clearInterval(tempo);
+//             window.location.reload();
+//         }
+//     }, 1000);
+// }
+// temporizador(300, "#content");
