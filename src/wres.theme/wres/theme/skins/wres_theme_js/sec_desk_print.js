73db0900ed27978b66c2fad89430fb2cb225ac76
Luiz Ferreira <lgustavo.fonseca@gmail.com>
2014-11-11 11:21:17 -0200

    Corrigindo Imprimir Agenda reportado pela Elaine Miwa

--- src/wres.theme/wres/theme/skins/wres_theme_js/sec_desk_print.js
+++ src/wres.theme/wres/theme/skins/wres_theme_js/sec_desk_print.js
@@ -1,14 +1,22 @@
 function secDeskPrint(){
-	if($(".selected")[1].id == "fieldsetlegend-tomorrow"){
-		hoje = $("#hoje-table")[0].outerHTML
-		$("#hoje-table").remove()
-		print()
-		$("#day-tables").append(hoje)
-	}
-	else{
-		amanha = $("#amanha-table")[0].outerHTML
-		$("#amanha-table").remove()
-		print()	
-		$("#day-tables").append(amanha)
-	}
+    
+    // copia o tbody da .visits_table visivel (pode ser Hoje ou Amanhã)
+    tbody_copy = $(".visits_table:visible").children("tbody").clone();
+
+    // print_table = $("#print-table");
+    print_table = $("table.cmedPrint");
+    print_table.children("tbody").remove(); // limpa qualquer tbody já existente
+    print_table.append(tbody_copy); // insere a cópia na tabela que será impressa
+
+    // Esconde/Mostra titulo dependendo se será impresso Hoje ou Amanhã
+    if($(".selected")[1].id == "fieldsetlegend-tomorrow"){
+        $("#today-title").addClass("visualNoPrint");
+        $("#tomorrow-title").removeClass("visualNoPrint");
+    }
+    else {
+        $("#tomorrow-title").addClass("visualNoPrint");
+        $("#today-title").removeClass("visualNoPrint");
+    }
+
+    print();
 }
