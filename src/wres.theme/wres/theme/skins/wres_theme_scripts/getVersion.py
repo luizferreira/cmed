ea5b19ad5066ef708fb6586b5c30ec2436d066c2
Luiz Ferreira <lgustavo.fonseca@gmail.com>
2016-01-13 17:08:43 -0200

    Data da última atualização calculada dinamicamente

--- src/wres.theme/wres/theme/skins/wres_theme_scripts/getVersion.py
+++ src/wres.theme/wres/theme/skins/wres_theme_scripts/getVersion.py
@@ -2,8 +2,13 @@ from Products.CMFCore.utils import getToolByName
 from wres.policy.utils.utils import getWresSite
 from DateTime import DateTime
 
-vt = getToolByName(context,"vocabulary_tool")
-Versao = vt.get_vocabulary("cmed_version")
-DataVersao = vt.get_vocabulary("cmed_data")
+today = DateTime()
+last_update = today - today.week() - 1
 
-return "Versão {versao} de {data}".format(versao="".join(Versao), data="".join(DataVersao))
+return "Última atualização em " + last_update.strftime('%d/%m/%Y')
+
+# vt = getToolByName(context,"vocabulary_tool")
+# Versao = vt.get_vocabulary("cmed_version")
+# DataVersao = vt.get_vocabulary("cmed_data")
+
+# return "Versão {versao} de {data}".format(versao="".join(Versao), data="".join(DataVersao))
